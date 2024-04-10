236a679a5b819d6f33abfb967ab4b8b463b0f84e
GustavoCR021 <gustavoc1922@gmail.com>
2024-04-09 23:20:18 -0300

    at

--- dist/js/index.js
+++ dist/js/index.js
@@ -4,3 +4,15 @@ let x = 10;
 const a = 121465;
 x = 12;
 console.log(a);
+// inferencia x annotation
+let y = 12; // inferencia
+let z = 12; // annotation
+// Tipos básicos
+let firstName = 'Matheus';
+let age = 20;
+const isAdmin = true;
+// String != string
+console.log(firstName);
+console.log(typeof firstName);
+firstName = 'Pedro';
+console.log(firstName);
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,26 @@ const a: number = 121465;
 x = 12;
 
 console.log(a)
+
+
+// inferencia x annotation
+
+let y = 12; // inferencia
+
+let z: number = 12; // annotation
+
+// Tipos básicos
+
+let firstName: string = 'Matheus'
+let age: number = 20
+const isAdmin: boolean = true
+
+
+// String != string
+
+console.log(firstName)
+console.log(typeof firstName)
+
+firstName = 'Pedro'
+
+console.log(firstName)
